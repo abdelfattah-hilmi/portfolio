620dc84065ca93cea33e6d8828495af097a273be
Abdelfattah Hilmi <hilmiabdelfattah@gmail.com>
2023-02-11 03:19:40 +0100

    Fixed import issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://pyhilmi.online/",
